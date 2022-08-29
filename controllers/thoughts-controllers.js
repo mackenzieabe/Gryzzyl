82d568b89e75320809e4810fe375d5d2423757e8
mackgabe <mackenzieabe@gmail.com>
2022-08-29 14:37:39 -0400

    added thought controller.js file

--- controllers/thoughts-controllers.js
+++ controllers/thoughts-controllers.js
@@ -2,7 +2,7 @@ const { Thoughts, User } = require('../models');
 
 //get all thoughts
 const thoughtsController = {
-    getAllThoughts(req, res) {
+    getThoughts(req, res) {
         Thoughts.find({})
             .select('-_v')
             .then(dbThoughtsData => res.json(dbThoughtsData))
@@ -11,19 +11,45 @@ const thoughtsController = {
             });
     },
 
-    getThoughtsById({ params }, res) {
-        Thoughts.findOne({ _id: params.id })
+    getSingleThought(req, res) {
+        Thoughts.findOne({ _id: params.userId })
             .select('-_v')
-            .then(dbThoughtsData => {
-                if (!dbThoughtsData) {
-                    res.status(404).json({ message: 'No thoughts found with this ID!' });
-                    return;
-                }
-                res.json(dbThoughtsData)
-            })
-            .catch(err => {
-                res.status(400).json(err)
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID!' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+
+    },
+    //POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+
+
+    updateThought({ params, body }, res) {
+        Thoughts.findOneAndUpdate(
+            { _id: params.id },
+            { $set: req.body },
+            { new: true, runValidators: true })
+            .then((thoughts) =>
+                !thoughts
+                    ? res.status(404).json({ message: 'No thought with this id!' })
+                    : res.json(thoughts)
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
             });
+    },
+
+    deleteThought(req, res) {
+        Thoughts.findOneAndDelete({ _id: req.params.thoughtsId })
+            .then((thoughts) =>
+                !thoughts
+                    ? res.status(400).json({ message: 'No thought found with this ID!' })
+                    : Thoughts.deleteMany({ _id: { $in: user } })
+            )
+
+            .then(() => res.json({ message: 'Thought deleted!' }))
+            .catch((err) => res.status(500).json(err));
     }
- 
-}
+};
--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -36,7 +36,7 @@ const userController = {
             .then((user) =>
             !user
               ? res.status(404).json({ message: 'No user with this id!' })
-              : res.json(use)
+              : res.json(user)
           )
           .catch((err) => {
             console.log(err);
